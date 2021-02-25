27d7468686135bca8f155840d38bca7d1c605f44
ringriggs <griggs117@gmail.com>
2021-02-25 10:44:05 -0800

    map, camera, ajax

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'A0or6qowJoUkyo6uku9oT2V1OifDIwx9';
+	
+	var map = L.mapquest.map('map', {
+		center: [32.8794378,-117.240223],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.8794378,-117.240223]).addTo(map);
 }
